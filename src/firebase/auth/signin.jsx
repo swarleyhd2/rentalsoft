bbf83672f6fae3033c42b5a0842390ce8dc958f5
chernan326 <chernan326@gmail.com>
2023-08-22 06:36:56 -0400

    first commit

--- src/firebase/auth/signin.jsx
+++ src/firebase/auth/signin.jsx
@@ -1,11 +1,15 @@
 import firebase_app from "../config";
-import {getAuth, GoogleAuthProvider, signInWithRedirect} from "firebase/auth";
+import {getAuth, getRedirectResult, GoogleAuthProvider, signInWithRedirect} from "firebase/auth";
 
 
 
 export default async function googleSignIn() {
     const provider = new GoogleAuthProvider();
     const auth = getAuth(firebase_app);
-    signInWithRedirect(auth, provider);
+    signInWithRedirect(auth, provider).then(
+        getRedirectResult(auth).then(
+            
+        )
+    )
 }
 
