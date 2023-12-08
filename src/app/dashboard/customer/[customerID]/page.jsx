c07781dd82be6b9dd64ac261055a7f03b02d60d8
chernan326 <chernan326@gmail.com>
2023-12-08 07:21:59 -0500

    accordian

--- src/app/dashboard/customer/[customerID]/page.jsx
+++ src/app/dashboard/customer/[customerID]/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { getCustomer } from '@/firebase/getData'
-import { Button, Card, Grid, Stack, Paper} from '@mui/material';
+import { Button, Card, Grid, Stack, Paper, Accordion, AccordionSummary, AccordionDetails} from '@mui/material';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import PlaceIcon from '@mui/icons-material/Place';
@@ -9,6 +9,7 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import Link from 'next/link';
 import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { ExpandMore } from '@mui/icons-material';
 
 export default function page({ params }) {
   const searchParams = useSearchParams()
@@ -16,32 +17,64 @@ export default function page({ params }) {
   const [ history, setHistory ] = useState()
 
   return (
-    <Grid container justifyContent='center'>
+    <Grid container justifyContent='center' minHeight='100vh'>
       <Grid item xs={12}>
       <Link href='/dashboard/customerselect'><Button variant='text'><ChevronLeftIcon fontSize='inherit'/>Customer Select</Button></Link>
       </Grid>
-      <Grid item xs={12} justifyContent='center'>
-        <h2>{name}</h2>
-      </Grid>
       <Grid item xs={10} md={8} lg={2}>
           <Stack spacing={2}>
+            <h2 style={{'text-align':'center'}}>{name}</h2>
             <Link href='#'><Button variant='contained' sx={{'width':'100%', 'font-weight':'bold', 'justifyContent': 'center'}}><CalendarMonthIcon fontSize='large'/> New Order</Button></Link>
             <Link href='#'><Button variant='contained' sx={{'width':'100%', 'font-weight':'bold'}}><RequestQuoteIcon fontSize='large'/> Create Quote</Button></Link>
             <Link href='#'><Button variant='contained' sx={{'width':'100%', 'font-weight':'bold'}}><PlaceIcon fontSize='large'/> Request Move</Button></Link>
             <Link href='#'><Button variant='contained' sx={{'width':'100%', 'font-weight':'bold'}}><ConstructionIcon fontSize='large'/> Request Service</Button></Link>
           </Stack>
       </Grid>
-      <Grid item xs={12}>
-        <h2>Open Quotes</h2>
-      </Grid>
-      <Grid item xs={12}>
-        <h2>On Rent</h2>
-      </Grid>
-      <Grid item xs={12}>
-        <h2>History</h2>
-      </Grid>
-      <Grid item xs={12}>
-        <h2>Recent Rates</h2>
+      <Grid item xs={12} justifyContent='center'>
+        <Grid item xs={12} md={8} lg={2} justifyContent='center'>
+          <Stack>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore/>}
+              >
+                Open Quotes
+              </AccordionSummary>
+              <AccordionDetails>
+                Open Quotes
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore/>}
+              >
+                Recent Rates
+              </AccordionSummary>
+              <AccordionDetails>
+               Recent Rates
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore/>}
+              >
+                On Rent
+              </AccordionSummary>
+              <AccordionDetails>
+                On Rent
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore/>}
+              >
+                History
+              </AccordionSummary>
+              <AccordionDetails>
+                History
+              </AccordionDetails>
+            </Accordion>
+          </Stack>
+        </Grid>
       </Grid>
     </Grid>
     
--- src/app/dashboard/customerselect/page.jsx
+++ src/app/dashboard/customerselect/page.jsx
@@ -5,6 +5,7 @@ import { Card, Grid, List, ListItem, ListItemButton, ListItemText } from "@mui/m
 import Link from "next/link";
 
 export default function page() {
+    const [ customerList, setCustomerList ] = useState([])
     const [ customerListItems, setCustomerListItems ] = useState()
     const [ newCustomerItem, setNewCustomerItem ] = useState()
     useEffect(() => {
@@ -23,7 +24,7 @@ export default function page() {
         <>
             <Grid container justifyContent='center'>
                 <Grid item xs={12} md={6} lg={4} sx={{'justifyContent':'center'}}>
-                    <Card>         
+                    <Card>    
                         <List>
                             {newCustomerItem}
                             {customerListItems}
